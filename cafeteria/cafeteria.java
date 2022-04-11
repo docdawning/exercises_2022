8ddababf52f4daeb5801b6913161c4a98d5db64e
James Snell <>
2022-04-10 22:37:39 -0600

    bad algo

--- cafeteria/cafeteria.java
+++ cafeteria/cafeteria.java
@@ -26,14 +26,32 @@ class Solution {
         
       //0 1 2 3 4 5 6 7 8 9 A B C D E
       //          x         x     x   
-       
+      
+      /*
+      0 1 2 3 4 5 6 7 8 9
+          x       x
+      
+      1. is i in S?
+      2. is i+(1..k) in S?
+      3. 
+  
+      
+      */
+      
       long new_occupants = 0;
       for (int seat=0;seat<N;seat++) {
+        //if 'seat' was already occupied, skip this one
+        if (array_contains_element(S, seat)) {
+          seat=seat+(int)K;
+          continue;
+        }
+        
         //decide if there are enough clear spaces to the right to seat someone
         if (seats_to_the_right_are_clear(N, seat, K, M, S)) {
           //are there enough clear seats to the right of that candidate seat as well?
           if (seats_to_the_right_are_clear(N, seat+K, K, M, S)) {
             new_occupants++;
+            seat=seat+(int)K;
           }
         }
           
